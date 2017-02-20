d6806a470e374dbc19da5990e1ba7a5d2872cdd5
zhengyanan <2821084381@qq.com>
2017-02-20 20:13:39 +0800

    testForPushCodesToGithubRespository

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/2/20.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("HelloWorld.main():" + "Hello World!");
+    }
+}
