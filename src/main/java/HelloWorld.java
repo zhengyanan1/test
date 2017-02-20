ce9713e9af7c9050e6f3c32672aa2f73715d5cf4
zhengyanan <2821084381@qq.com>
2017-02-20 20:27:31 +0800

    a

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,5 +4,6 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("HelloWorld.main():" + "Hello World!");
+        System.out.println("HelloWorld.main():" + "Let's go!");
     }
 }
